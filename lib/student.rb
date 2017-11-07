d99ccb6f8f07cd03b03e31ff817061e42bbf40db
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-07 14:58:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ class Student
   end
 
   def save
-    DB[:conn].execute("insert into students (name, grade) values (#{self.id}, '#{self.name}', #{self.grade})")
+    DB[:conn].execute("insert into students (name, grade) values ('#{self.name}', #{self.grade})")
   end
 
 end
